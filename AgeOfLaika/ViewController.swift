0f914e1072c401111a0bca75cfba4467e1db3207
Craig Martin <cmart1220@gmail.com>
2017-04-19 20:25:56 -0400

    Migrate Local Copy to GitHub

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayDogYears: UILabel!
+    @IBOutlet weak var enterHumanYearsText: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertDogYearsPressed(sender: UIButton) {
+        let humanAgeText = enterHumanYearsText.text
+        var dogAgeYears = humanAgeText.toInt()!
+        let dogAgeConversionConstant = 7
+        dogAgeYears *= dogAgeConversionConstant
+        displayDogYears.text = "\(dogAgeYears)"
+        enterHumanYearsText.text = ""
+        enterHumanYearsText.resignFirstResponder()
+    }
 
+    
+    @IBAction func convertRealDogYearsPressed(sender: UIButton) {
+        let humanAgeText = enterHumanYearsText.text
+        var dogRealAgeYears = Double((humanAgeText as NSString).doubleValue)
+        if dogRealAgeYears <= 2.0 {
+           let dogRealAgeConversionConstantYounger = 10.5
+            dogRealAgeYears *= dogRealAgeConversionConstantYounger
+        }
+        else {
+           let dogRealAgeConversionConstantOlder = 4.0
+            dogRealAgeYears = 21.0 + ((dogRealAgeYears-2.0)*dogRealAgeConversionConstantOlder)
+        }
+        displayDogYears.text = "\(dogRealAgeYears)"
+        enterHumanYearsText.text = ""
+        enterHumanYearsText.resignFirstResponder()
+    }
+    
 }
 
